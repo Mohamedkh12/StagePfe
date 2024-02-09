6fa0b1f1a3a3f411c1799893aba35c4bf1ad496e
Mohamedkh12 <mohamed.khalil.khelifi@horizon-tech.tn>
2024-02-09 21:32:40 +0100

    create a payment page

---
+++ InscriptionParent/CompteParent.styles.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    h1:{
+        fontSize: 26,
+        fontFamily: 'bold',
+        color:"#293772",
+        lineHeight:22,
+    }
+})
+
+export default styles
